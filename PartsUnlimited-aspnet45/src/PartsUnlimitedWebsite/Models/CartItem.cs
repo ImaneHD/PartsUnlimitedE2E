b4845a142e994fba53a66bb7c5fafe645f61ed39
ImaneHD <imanehadnane@gmail.com>
2023-06-19 23:03:34 +0100

    my commit

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        //My second change
+        //My first commit
         [Key]
         public int CartItemId { get; set; }
 
